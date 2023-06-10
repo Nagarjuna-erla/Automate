4a8a82ea7a8b8a612fc0d86fa10afbcf6f967733
Busy_Man <57696289+Nagarjuna-erla@users.noreply.github.com>
2023-06-11 01:38:55 +0530

    Update Two_Num_Sum_Format.py

--- Two_Num_Sum_Format.py
+++ Two_Num_Sum_Format.py
@@ -3,4 +3,4 @@ num = input("Enter first Number:")
 num2 = input("Enter second Number:")
 sum = num + num2
 print(" The sum of {0} and {1} is : {2}".format(num,num2,sum)
-print("The sum of %d and %d is:%d"%(num1,num2,sum))             
+#print(" The sum of %d and %d is:%d",%(num1,num2,sum))             
